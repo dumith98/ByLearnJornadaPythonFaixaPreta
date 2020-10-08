1d32031ed54406a278eab55efb76b3496451e689
Dumith Bou-Habib <72422410+dumith98@users.noreply.github.com>
2020-10-08 12:12:54 -0300

    Add files via upload

---
+++ queda_livre.py
@@ -0,0 +1,29 @@
+
+
+gravidade = 9.8
+
+def calculo_tempo(altura):
+    
+    tempo = ((2*altura)/gravidade)**(1/2)
+    print('O tempo que leva para o objeto chegar ao solo é: ', tempo, 'segundos.')
+
+def calculo_altura(tempo):
+    
+ 
+    altura = (gravidade * pow(tempo, 2)) / 2
+    print('Se o objeto cair por', tempo, 'segundos, ele teria sido largado de uma altura igual a ', altura, 'metros.')
+
+print('Bem vindo a e obrigado por utilizar a minha calculadora de queda livre!')
+print('--------------------')
+print('Vale lembrar que estamos desconsiderando a resistencia do are considerando a gravidade igual a 9.8 m/s²!')
+print('--------------------')
+escolha = input('Escolha o que quer descobrir, altura ou tempo?: ')
+
+if escolha == 'tempo':
+    altura = float(input('Informe a altura da qual o objeto foi largado em metros: '))    
+    calculo_tempo(altura)
+
+elif escolha == 'altura':
+    tempo = float(input('Informe por quanto tempo o objeto vai cair em segundos: '))
+    calculo_altura(tempo)
+
