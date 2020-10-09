17212654ba177147c25858e667459d28b2bfa2dc
Dumith Bou-Habib <72422410+dumith98@users.noreply.github.com>
2020-10-09 20:28:40 -0300

    Update queda_livre.py

--- queda_livre.py
+++ queda_livre.py
@@ -1,3 +1,4 @@
+
 gravidade = 9.8
 
 def calculo_tempo(altura):
@@ -15,13 +16,17 @@ print('Bem vindo a e obrigado por utilizar a minha calculadora de queda livre!')
 print('--------------------')
 print('Vale lembrar que estamos desconsiderando a resistencia do are considerando a gravidade igual a 9.8 m/s²!')
 print('--------------------')
+
+
 escolha = input('Escolha o que quer descobrir, altura ou tempo?: ')
 
+ 
+while escolha != 'tempo' and escolha != 'altura':   
+    print('Voce deve ter digitado algo errado. Tente de novo.')
+    escolha = input('Escolha o que quer descobrir, altura ou tempo?: ') 
 if escolha == 'tempo':
     altura = float(input('Informe a altura da qual o objeto foi largado em metros: '))    
     calculo_tempo(altura)
-
 elif escolha == 'altura':
     tempo = float(input('Informe por quanto tempo o objeto vai cair em segundos: '))
     calculo_altura(tempo)
-
